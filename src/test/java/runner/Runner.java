b7113f1e43f58d80b2fde8a319d41c9ec28d16ee
Ankit Mistry <aenkymistry26@gmail.com>
2020-05-15 15:13:03 +0530

    Tesbo demo with lambda test

---
+++ src/test/java/runner/Runner.java
@@ -0,0 +1,12 @@
+package runner;
+
+import Execution.Tesbo;
+
+public class Runner {
+    public static void main(String[] args) {
+
+        Tesbo tests = new Tesbo();
+        tests.run(args);
+    }
+}
+
